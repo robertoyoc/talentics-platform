eadfe114cf16fe2ad2c2e89c6bbd247866eb6cbf
emilio <emitrax72@gmail.com>
2018-06-09 12:22:20 -0500

    Añadi modelos questioon, kit request, advisory y report

--- app/models/advisory.js
+++ app/models/advisory.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	classroom: DS.belongsTo('classroom'),
+	chat: DS.belongsTo('chat'),
+	videollamadas: DS.hasMany('videocall')
 
 });
--- app/models/report.js
+++ app/models/report.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	reportador: DS.attr('string'),
+	reportado: DS.attr('string'),
+	texto: DS.attr('string')
 
 });
