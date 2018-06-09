c848d385f5b79a0931a41a53c3d01f5c2ac47dc7
ctellezesp <alemiguel97@gmail.com>
2018-06-09 12:29:37 -0500

    added attributes and relations on models certification, group-class, kid, message, schedule

--- app/models/group-class.js
+++ app/models/group-class.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	kid: DS.hasMany('kid'),
+	profesor: DS.belongsTo('profesor')
+
 
 });
--- app/models/kid.js
+++ app/models/kid.js
@@ -1,5 +1,19 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	
+	nombre: DS.attr('string'),
+	apellido: DS.attr('string'),
+	fechaNacimiento: DS.attr('number'),
+	padre: DS.belongsTo('father'),
+	tutoringRequest: DS.hasMany('tutoring-request'),
+	duda: DS.hasMany('doubt'),
+	salon: DS.hasMany('classroom')
+	areaInteres: DS.hasMany('interest-area'),
+	claseGrupal: DS.hasMany('group-class'),
+	cusro: DS.hasMany('course'),
+	test: DS.hasMany('test'),
+	beca: DS.hasMany('scholarship')
+
 
 });
