84aa4f757be7aa215a5ebe8ff6eab0f1e7cda959
joeduardo.arteaga@gmail.com <joshuaesperilla.99@gmail.com>
2018-06-09 12:18:10 -0500

    Coloque atributos y relaciones de: job, teacher, interestArea, owner, videocall

--- app/models/interest-area.js
+++ app/models/interest-area.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+	nino: DS.belongsTo('kid'),
+	nombreArea: DS.attr('string'),
+	descripcionArea: DS.attr('string'),
 });
--- app/models/teacher.js
+++ app/models/teacher.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+	nombreProfesor: DS.attr('string'),
+	apellidoProfesor: DS.attr('string'),
+	trabajo: DS.hasMany('job'),
+	especializacion: DS.attr('string'),
 });
