09ed84a282b4c37d4b9b7df45fc1d9062aeb9317
Kevin G <keveggts@gmail.com>
2018-11-06 17:55:31 -0600

    muestra solicitudes de asesorias del profesor

--- app/routes/profesor/asesorias/solicitudes.js
+++ app/routes/profesor/asesorias/solicitudes.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	model(){
+		let cuenta=this.get('currentUser.account.infoID');
+		return this.store.query('tutoringRequest', {
+            teacher: cuenta,
+          })
+	}
 });
